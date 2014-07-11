5946f1354d17c689a8875d61e96c1a3f89d1b7ed
Phil Thompson <hello@philthompson.co.uk>
2014-07-11 11:19:55 +0100

    intial copy added along with favicon, body copy font and .htaccess file

--- index.php
+++ index.php
@@ -6,40 +6,84 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Pets Love Summer - Out &amp; About Roadshow</title>
+	<title>Freelance Front-end Developer Bristol - Phil Thompson</title>
 	<script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/,'js');</script>
 	<!--[if lt IE 9]> <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
-	<meta name="viewport" content="initial-scale=1.0">
-	<title>Freelance Front-end Developer Bristol - Phil Thompson</title>
+	<meta name="viewport" content="initial-scale=1.0,minimal-ui">
+	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 <div id="site-container">
-	<div class="section">
-		<h1><a href="http://imgiseverything.co.uk/">Phil Thompson</a> is a <a href="http://imgiseverything.co.uk/work/">freelance front-end developer</a> based in the South-west of England.</h1>
+	<div class="group section">
+		<div class="inner">
+			<h1><a href="http://imgiseverything.co.uk/">Phil Thompson</a> is a <a href="http://imgiseverything.co.uk/work/">freelance front-end developer</a> based in the South-west of England.</h1>
+		</div>
+	</div>
+	<div class="group section">
+		<div class="inner">
+			<h2>What he does</h2>
+			<p>Phil specialises in Front-end web code and WordPress. For those of you who don’t speak geek, this means he converts web designs (usually from Photoshop) into fully-functioning websites built upon the incredibly easy-to-use WordPress platform.</p>
 
+			<p>His professional experience has enabled Phil to gain a very good understanding of how websites work, from how they initially attract people to how they convince those same people to purchase/read/subscribe and subsequently return. Phil has a proven record of improving the fortunes of websites, whether that means increasing profits or increasing their findability in search engines.</p>
+		</div>
 	</div>
-	<div class="section">
-		
+	<div class="group section">
+		<div class="inner">
+			<h2>Examples and work history</h2>
+
+			<p>Through his work with digital agencies, Phil has worked with some large global and national brands including Playstation, The Rolling Stones, Boots, EA Games. This work is usually as part of a team.</p>
+			
+			<p>For those interested in some of the projects Phil has been involved in, there is an online portfolio and for those interested in reading about what Phil has done in his career so far/his skills, there is an online CV. You can also see Phil’s Linkedin profile to see more about him work-wise.</p>
+			
+			<p>Phil is available for freelance/contract work. If you’re a digital/design agency why not get in touch to see what Phil can offer.</p>
+		</div>
 	</div>
-	<div class="section">
-		
+	<div class="group section">
+		<div class="inner">
+			<h2>Personal life</h2>
+			<p>Phil has lived in Manchester for over 12 years and currently resides in the Whalley Range area of Manchester with his two housemates with whom he likes to watch (terrible, but at the same time awesome) Jason Statham films.</p>			
+			<p>He cycles to work which keeps him fit and allows him to feel like an environmentally-aware superhero; on rainy days (of which there are many in Manchester) he wears some rather fetching waterproof cycling trousers and a bright yellow rain mac – so don’t worry about him.</p>		
+		</div>
 	</div>
-	<div class="section">
-		
+	<div class="group section">
+		<div class="inner">
+			<h2>Chosen charities</h2>
+			<p>Currently Phil&#8217;s chosen charities are:</p>
+			<ul>
+				<li><a href="http://justgiving.com/sparks">Sparks</a>, fund life-saving medical research into conditions affecting babies, children and mums-to-be. Phil is riding the 2014 l&#8217;Etape du Tour for Sparks. <a href="https://www.justgiving.com/etape-for-sparks/">Sponsor him here</a>.</li>
+				<li><a href="http://www.christie.nhs.uk/">The Christie</a>, a Manchester-based Cancer centre that treats over 40,000 patients a year. Phil currently donates £600 a year.</li>
+				<li><a href="http://www.woodstreetmission.org.uk/">Wood Street Mission</a>, a Manchester-based charity providing practical help to children and their families. Phil currently donates £180 a year.</li>
+				<li><a href="http://www.shelter.org.uk/">Shelter</a>, a national UK charity working with homeless people. Phil currently donates £180 a year.</li>
+				<li><a href="http://www.boothcentre.org.uk/">Booths Centre</a>, a day centre that offers advice, activities and support to homeless people in Manchester. Phil currently donates £180 a year.</li>
+			</ul>	
+		</div>
+	</div>	
+	<div class="group section">
+		<div class="inner">
+			<h2>What’s with the yellow?</h2>
+			<p>The short answer is because Phil likes loves it; the longer answer is that it makes the website stand-out. It allows for a very simplistic, some may say, minimalist design that maintains some character.</p>			
+			<p>Some people like the yellow and some don’t.</p>			
+		</div>
+	</div>			
+	<div class="group section">
+		<div class="inner">
+			<ul class="block-list social-list">
+				<li><a href="http://last.fm/user/imgiseverything" class="icon ss-lastfm">Last FM</a></li>
+				<li><a href="http://linkedin.com/in/philt" class="icon ss-linkedin">LinkedIn</a></li>
+				<li><a href="http://twitter.com/imgiseverything" class="icon ss-twitter">Twitter</a></li>
+				<li><a href="http://open.spotify.com/user/imgiseverything" class="icon ss-spotify">Spotify</a></li>
+				<li><a href="http://instagram.com/imgiseverythin9" class="icon ss-instagram">Instagram</a></li>
+				<li><a href="http://www.strava.com/athletes/340958" class="icon ss-link">Strava</a></li>
+				<li><a href="https://pinboard.in/u:imgiseverything" class="icon ss-pinboard">Pinboard</a></li>
+				<li><a href="http://pinterest.com/imgiseverything" class="icon ss-pinterest">Pinterest</a></li>
+				<li><a href="http://github.com/imgiseverything" class="icon ss-github">Github</a></li>
+				<li><a href="https://soundcloud.com/imgiseverythin9" class="icon ss-soundcloud">Soundcloud</a></li>
+			</ul>
+		</div>
 	</div>
-	<div class="section">
-		
-	</div>
-	<div class="section">
-		
-	</div>
+
 </div>
-<ul id="social">
-	<li><a href="http://www.linkedin.com/in/philt">Linkedin</a></li>
-	<li><a href="http://www.twitter.com/imgiseverything">Twitter</a></li>
-	<li><a href="http://last.fm/user/imgiseverything">Last.fm</a></li>
-	<li><a href="http://imgiseverything.co.uk/">Blog</a></li>
-</ul>
+
 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
