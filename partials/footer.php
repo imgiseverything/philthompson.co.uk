16e3c511676f04f0f2a51d592a6cba397bc27ac9
Phil Thompson <hello@philthompson.co.uk>
2015-12-01 14:19:01 +0000

    Removed render-blocking javascripts
    A script now loads in all relevant scripts

--- partials/footer.php
+++ partials/footer.php
@@ -1,17 +1,34 @@
 <progress class="progress-bar js-progress-bar" value="0" max="100"></progress>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-2.1.3.min.js"><\/script>')</script>
-<?php 
-	// In production use a minified JS file
-	if(strpos($_SERVER['HTTP_HOST'], '.co.uk') !== false): 
-?>
-	<script src="/assets/js/philthompsoncouk.<?php echo $updated; ?>.min.js" async></script>
-<?php else: ?>
-	<script src="/assets/js/philthompsoncouk.js" async></script>
-<?php endif; ?>
+<script>
+	// Request and load scripts after page load (IE9+)
+	function loadPTScripts(){
 
-	<script src="/assets/js/vendor/svg4everybody.js"></script>
-	<script>svg4everybody(); // run it now or whenever you are ready</script>
+		// jQuery
+		var scriptjQuery = document.createElement('script'),
+				scriptPT = document.createElement('script'),
+				scriptSVG4everybody = document.createElement('script');
+
+		scriptjQuery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
+		document.body.appendChild(scriptjQuery);
+
+		// Load scripts reliant on jQuery
+		scriptjQuery.onload = function(){
+			scriptPT.src = '/assets/js/philthompsoncouk.<?php echo $updated; ?>.min.js';
+			document.body.appendChild(scriptPT);
+		}
+
+		// SVG4everybody - get IE9-11 to allow external SVG sprites
+		scriptSVG4everybody.src = '/assets/js/vendor/svg4everybody.js';
+		document.body.appendChild(scriptSVG4everybody);
+		scriptSVG4everybody.onload = function(){
+			svg4everybody(); // run it now or whenever you are ready	
+		}
+	}
+
+	if(window.addEventListener){
+		window.addEventListener('load', loadPTScripts, false);
+	}
+</script>
 </body>
 </html>
