7ed7a582fc14e0376786af9322c425d3c1bb7e4a
Phil Thompson <=>
2021-10-21 10:09:18 +0100

    chore: Upgrade outdated dependencies, remove postCSS and upgrade to dart-sass from node-sass

--- _grunt/Gruntfile.js
+++ _grunt/Gruntfile.js
@@ -1,49 +1,46 @@
+const sass = require("dart-sass");
 
-
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
 	// load all grunt tasks
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 
 		// Combine JavaScript files into one big file
 		concat: {
 			options: {
-				separator: ';'
+				separator: ";",
 			},
 			dist: {
 				src: [
-					'!../assets/js/vendor/*.js',
-					'../assets/js/plugins/*.js',
-					'../assets/js/modules/*.js',
-					'!../assets/js/<%= pkg.name %>.js',
-					'!../assets/js/<%= pkg.name %>.min.js'
+					"!../assets/js/vendor/*.js",
+					"../assets/js/plugins/*.js",
+					"../assets/js/modules/*.js",
+					"!../assets/js/<%= pkg.name %>.js",
+					"!../assets/js/<%= pkg.name %>.min.js",
 				],
-				dest: '../assets/js/<%= pkg.name %>.js'
-			}
+				dest: "../assets/js/<%= pkg.name %>.js",
+			},
 		},
 
 		// Obfuscate and minfiy JavaScript to save filesize
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+				banner:
+					'/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
 			},
 			dist: {
 				files: {
-					'../assets/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-				}
-			}
+					"../assets/js/<%= pkg.name %>.min.js": ["<%= concat.dist.dest %>"],
+				},
+			},
 		},
 
 		// Run JavaScrit through JSLint to maintain coding standards
 		jslint: {
-
 			client: {
-				src: [
-					'../assets/js/modules/*.js'
-				],
+				src: ["../assets/js/modules/*.js"],
 				directives: {
 					browser: true,
 					devel: true,
@@ -52,142 +49,131 @@ module.exports = function(grunt) {
 					unparam: true,
 					unused: true,
 					plusplus: true,
-					predef: [
-						'debounce',
-						'jQuery',
-						'smoothScroll',
-						'throttle',
-						'window'
-					]
+					predef: ["debounce", "jQuery", "smoothScroll", "throttle", "window"],
 				},
-				options: {
-				}
-			}
+				options: {},
+			},
 		},
 
 		// Karma
 		karma: {
 			unit: {
-				configFile: 'karma.conf.js'
-			}
+				configFile: "karma.conf.js",
+			},
 		},
 
 		// Convert Sass to CSS
-		sass: {	 // Task
-			dist: {	 // Target
-				files: { // Dictionary of files
-					'../assets/css/style.css': '../_sass/style.scss'		 // 'destination': 'source'
-				}
-			}
-		},
-
-		// Add browser prefixes to CSS and minify it
-		postcss: {
-			options: {
-				map: false, // don't inline sourcemaps
-				processors: [
-					require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-					require('cssnano')() // minify the result
-				]
+		"dart-sass": {
+			// Task
+			target: {
+				options: {
+					outputStyle: "compressed",
+					sourceMap: false,
+				},
+				// Target
+				files: {
+					// Dictionary of files
+					"../assets/css/style.css": "../_sass/style.scss", // 'destination': 'source'
+				},
 			},
-			dist: {
-				src: '../assets/css/*.css'
-			}
 		},
 
 		// Optimise Images
 		imagemin: {
 			png: {
 				options: {
-					optimizationLevel: 7
+					optimizationLevel: 7,
 				},
 				files: [
 					{
 						// Set to true to enable the following options…
 						expand: true,
 						// cwd is 'current working directory'
-						cwd: '../assets/images/',
-						src: ['**/*.png'],
+						cwd: "../assets/images/",
+						src: ["**/*.png"],
 						// Could also match cwd line above. i.e. project-directory/img/
-						dest: '../assets/images/',
-						ext: '.png'
-					}
-				]
+						dest: "../assets/images/",
+						ext: ".png",
+					},
+				],
 			},
 			jpg: {
 				options: {
-					progressive: true
+					progressive: true,
 				},
 				files: [
 					{
 						// Set to true to enable the following options…
 						expand: true,
 						// cwd is 'current working directory'
-						cwd: '../assets/images/',
-						src: ['**/*.jpg'],
+						cwd: "../assets/images/",
+						src: ["**/*.jpg"],
 						// Could also match cwd. i.e. project-directory/img/
-						dest: '../assets/images/',
-						ext: '.jpg'
-					}
-				]
-			}
+						dest: "../assets/images/",
+						ext: ".jpg",
+					},
+				],
+			},
 		},
 
 		// SVG Store
 		// combine SVGs into a sprite - make sure SVGs are optimised manually
 		svgstore: {
 			options: {
-				prefix : 'symbol-', // This will prefix each <g> ID
+				prefix: "symbol-", // This will prefix each <g> ID
 			},
-			default : {
+			default: {
 				files: {
-					'../assets/images/svg-sprite.svg': ['../assets/images/icons/*.svg', '../assets/images/logos/*.svg'],
-				}
-			}
+					"../assets/images/svg-sprite.svg": [
+						"../assets/images/icons/*.svg",
+						"../assets/images/logos/*.svg",
+					],
+				},
+			},
 		},
 
 		// WATCH:
 		// Whenever a file is changed, run specific tasks
 		watch: {
 			css: {
-				files: [
-					'../_sass/**/*.scss',
-				],
-				tasks: ['sass', 'postcss'],
+				files: ["../_sass/**/*.scss"],
+				tasks: ["sass", "postcss"],
 				options: {
-					nospawn: true
-				}
+					nospawn: true,
+				},
 			},
 			js: {
 				files: [
-					'../**/*.js',
+					"../**/*.js",
 					// Ignore these
-					'!../js/<%= pkg.name %>.js',
-					'!../js/<%= pkg.name %>.min.js'
+					"!../js/<%= pkg.name %>.js",
+					"!../js/<%= pkg.name %>.min.js",
 				],
-				tasks: ['jslint', 'concat', 'uglify']
-			}
-		}
+				tasks: ["jslint", "concat", "uglify"],
+			},
+		},
 	});
 
 	// Load NPM modules
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-postcss');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-jslint');
-	grunt.loadNpmTasks('grunt-karma');
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks('grunt-phantom');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-svgstore');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks("grunt-dart-sass");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-jslint");
+	grunt.loadNpmTasks("grunt-contrib-imagemin");
+	grunt.loadNpmTasks("grunt-svgstore");
+	grunt.loadNpmTasks("grunt-contrib-watch");
 
 	// Tasks
-	grunt.registerTask('default', ['sass', 'postcss', 'concat', 'uglify', 'jslint', 'imagemin', 'svgstore']);
-	grunt.registerTask('css', ['sass', 'postcss']);
-	grunt.registerTask('js', ['concat', 'uglify', 'jslint']);
-	grunt.registerTask('images', ['imagemin', 'svgstore']);
-
+	grunt.registerTask("default", [
+		"dart-sass",
+		"concat",
+		"uglify",
+		"jslint",
+		"imagemin",
+		"svgstore",
+	]);
+	grunt.registerTask("css", ["dart-sass"]);
+	grunt.registerTask("js", ["concat", "uglify", "jslint"]);
+	grunt.registerTask("images", ["imagemin", "svgstore"]);
 };
